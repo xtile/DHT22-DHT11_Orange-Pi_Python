4e4b77a5d4780fd5c9f4eca19160478d2ea50566
Viacheslav Karpizin <xtile@yandex.ru>
2017-10-14 23:40:33 +0300

    fix: - correct port - error type output - cycle for infinite read

--- dht_example.py
+++ dht_example.py
@@ -6,11 +6,11 @@ import time
 
 # initialize GPIO
 DHT22_PIN = port.PA13
-DHT11_PIN = port.PA2
+DHT11_PIN = port.PA6 # PWM port
 gpio.init()
-#gpio.cleanup()
 
-DHT22_instance = dht.DHT22(pin=DHT22_PIN)
+
+#DHT22_instance = dht.DHT22(pin=DHT22_PIN)
 DHT11_instance = dht.DHT11(pin=DHT11_PIN)
 
 def DHT22_print_data(DHT22_result):
@@ -27,12 +27,16 @@ def DHT11_print_data(DHT11_result):
     if DHT11_result.is_valid():
         print 'Temperature: {}, Humidity: {}'.format(DHT11_result.temperature, DHT11_result.humidity)
     else:
-        DHT11_read_sensor()
+        print 'UNKNOWN error: ' + str(DHT11_result.error_code)
+        DHT11_read_sensor() # in case of error just reread port (fixme - stackoverflow?)
 
 def DHT11_read_sensor():
     DHT11_result = DHT11_instance.read()
     DHT11_print_data(DHT11_result)
 
 if __name__ == '__main__':
-    DHT22_read_sensor()
-    DHT11_read_sensor()
+#    DHT22_read_sensor()
+
+    while True:
+        time.sleep(2)
+        DHT11_read_sensor()
